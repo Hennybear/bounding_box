bb78e24590fd688bf0344258ba13a81bc5374b3f
HENNY <guerrahenny@gmail.com>
2017-02-08 15:55:30 -0800

    passed all unit tests in bounding_box_spec.rb

--- lib/bounding_box.rb
+++ lib/bounding_box.rb
@@ -1,2 +1,37 @@
 class BoundingBox
+  attr_reader :width, :height
+
+  def initialize(x, y, width, height)
+    @width = width
+    @height = height
+    @x = x
+    @y = y
+  end
+
+  def left
+    @x
+  end
+
+  def right
+    @x + @width
+  end
+
+  def bottom
+    @y
+  end
+
+  def top
+    @y + @height
+  end
+
+  def contains_point?(x, y)
+    if (x >= @x && x <= (@x + @width)) && (y >= @y && y <= @y + @height)
+      true
+    else
+      false
+    end
+
+  end
+
+
 end
