b4ec9cfd1aa5ea7df32d04018f51b90ea837f179
HENNY <guerrahenny@gmail.com>
2017-02-09 13:13:43 -0800

    Passed all unit tests in 02_bounding_area_spec.rb

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,15 @@
 class BoundingArea
+  def initialize(boxes)
+    @boxes = boxes
+  end
+
+  def boxes_contain_point?(x, y)
+    result = false
+    @boxes.each do |box|
+      if box.contains_point?(x, y) == true
+        result = true
+      end
+    end
+    result
+  end
 end
